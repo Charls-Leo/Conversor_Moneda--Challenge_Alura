b59447bd9944717d4c1d01740a277fcd88ac3559
Charls-Leo <leonardohernandezb1105@gmail.com>
2026-02-19 16:47:24 -0500

    Se agrega librería Gson manualmente

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,8 +1,12 @@
 package principal;
+import com.google.gson.Gson;
+
 
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Bienvenido al Conversor de Monedas");
+
+        Gson gson = new Gson();
+        System.out.println("Gson configurado correctamente");
     }
 }
 
